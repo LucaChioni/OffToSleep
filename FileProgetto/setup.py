219e7c6388a9cb7b71dee39a255f450cace0246c
Luca <luca.chioni46@gmail.com>
2022-08-25 20:13:16 +0200

    Corretti script per la creazione dell'eseguibile per un bug che potrebbe presentarsi con aggiornamenti dei SO di linux; aggiunto file per installare più rapidamente le dipendenze (per quando installi l'ambiente su altre macchine).

--- FileProgetto/setup.py
+++ FileProgetto/setup.py
@@ -1,7 +1,22 @@
 from cx_Freeze import setup, Executable
+
+sistema_operativo = "Windows"
+
+lista_librerie_da_includere = ["os", "sys", "gc", "psutil", "datetime", "pygame", "random", "copy", "math", "hashlib"]
+lista_shared_lib_da_includere = []
+if sistema_operativo == "Windows":
+    lista_librerie_da_includere.extend(["win32gui", "win32con", "ctypes"])
+elif sistema_operativo == "Linux":
+    lista_librerie_da_includere.extend(["encodings", "subprocess"])
+    lista_shared_lib_da_includere.extend(["libffi.so.8", "libffi.so.8.1.0"])
+build_exe_options = {
+    "packages": lista_librerie_da_includere,
+    "bin_includes": lista_shared_lib_da_includere
+}
 setup(
-    name="Gioco",
+    name="Off to Sleep",
     version="1.0",
-    description="un gran gioco",
+    description="A game...",
+    options={"build_exe": build_exe_options},
     executables=[Executable("FileProgetto/OffToSleep.py")]
 )
